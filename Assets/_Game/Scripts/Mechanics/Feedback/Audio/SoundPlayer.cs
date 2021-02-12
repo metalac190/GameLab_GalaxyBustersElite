279f0557f57fe03e869b9b05949198f1d8963e1c
Sammy Mahmoudi <bagelmaster5001@gmail.com>
2021-02-12 17:14:44 -0600

    Added four basic scripts (playing particles + sounds, shaking camera)

---
+++ Assets/_Game/Scripts/Mechanics/Feedback/Audio/SoundPlayer.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SoundPlayer : MonoBehaviour
+{
+    public bool playOnAwake;
+
+    public Sound sound;
+
+
+    void Awake()
+    {
+        if (sound == null)
+        {
+            Debug.LogWarning("SoundPlayer attached to " + transform.parent.name + " not given a sound to play.");
+            return;
+        }
+
+        sound.audioSource = gameObject.AddComponent<AudioSource>();
+        sound.audioSource.clip = sound.audioClip;
+        sound.audioSource.volume = sound.volume;
+        sound.audioSource.loop = sound.loop;
+        if (playOnAwake)
+            sound.audioSource.Play();
+    }
+
+
+    [ContextMenu("Test Play")]
+    public void Play() => sound.audioSource.Play();
+
+
+    [ContextMenu("Test Play Detach and Destroy")]
+    public void PlayThenDetachAndDestroy()
+    {
+        if (sound.loop)
+        {
+            Debug.LogWarning("SoundPlayer attached to " + transform.parent.name + " trying to detach and destroy with a looping sound.");
+            Play();
+            return;
+        }
+
+        transform.parent = null;
+        sound.audioSource.Play();
+        StartCoroutine(DestroyWhenFinished());
+    }
+
+    IEnumerator DestroyWhenFinished()
+    {
+        while (sound.audioSource.isPlaying)
+            yield return null;
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/_Game/Scripts/Mechanics/Feedback/Camera/CameraShaker.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class CameraShaker : MonoBehaviour
+{
+    float shakeAmt;
+    const float SHAKE_ENDING_SMOOTHNESS = 20;
+    const float NOT_SHAKING_THRESHOLD = 0.01f;
+
+
+    private void Update() => Shake();
+    private void Shake()
+    {
+        if (shakeAmt > 0)
+            transform.localPosition = Vector2.up * Random.Range(-shakeAmt, shakeAmt) + Vector2.right * Random.Range(-shakeAmt, shakeAmt);
+        else
+            transform.localPosition = Vector2.zero;
+    }
+
+    private void FixedUpdate() => DecrementShakeIfStoppedShaking();
+    private void DecrementShakeIfStoppedShaking()
+    {
+        if (shakeAmt > NOT_SHAKING_THRESHOLD)
+        {
+            shakeAmt = Mathf.Lerp(shakeAmt, 0, 1 / SHAKE_ENDING_SMOOTHNESS);
+            if (shakeAmt <= NOT_SHAKING_THRESHOLD)
+                shakeAmt = 0;
+        }
+    }
+
+    public void Shake(float setShakeAmt) { shakeAmt = setShakeAmt; }
+}
