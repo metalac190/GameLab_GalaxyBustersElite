48a5c45dae4465856a22586a15116ac6914b186b
Sammy Mahmoudi <bagelmaster5001@gmail.com>
2021-02-20 11:12:36 -0600

    ParticlesPlayer and SoundPlayer now support multiple sounds.  Feedback test scene

---
+++ Assets/_Game/Scenes/Test Scenes/Sammy Feedback Test Scene/TestDestroy.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class TestDestroy : MonoBehaviour
+{
+    public UnityEvent OnDestroyed;
+
+    [ContextMenu("Destroy Self")]
+    public void DestroySelf()
+    {
+        OnDestroyed.Invoke();
+        Destroy(this.gameObject);
+    }
+}
--- Assets/_Game/Scripts/Mechanics/Feedback/Particles/ParticlesPlayer.cs
+++ Assets/_Game/Scripts/Mechanics/Feedback/Particles/ParticlesPlayer.cs
@@ -1,35 +1,77 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class ParticlesPlayer : MonoBehaviour
 {
-    public bool playOnAwake;
-
-    public ParticleSystem particles;
+    [SerializeField] Particles[] allParticles = new Particles[1];
 
 
     private void Awake()
     {
-        if (playOnAwake)
-            particles.Play();
+        foreach (Particles particles in allParticles)
+            if (particles.playOnAwake)
+                particles.particleSystem.Play();
     }
 
     private void Start()
     {
-        if (particles == null)
+        if (allParticles.Length == 0)
             Debug.LogWarning("ParticlesPlayer attached to " + transform.parent.name + " not given any particles to play.");
     }
 
 
-    [ContextMenu("Test Play")]
-    public void Play() => particles.Play();
 
-
-    [ContextMenu("Test Play Detach and Destroy")]
-    public void PlayThenDetachAndDestroy()
+    public void Play(int indexParticlesToPlay)
     {
-        transform.parent = null;
-        var main = particles.main;
-        main.stopAction = ParticleSystemStopAction.Destroy;
-        particles.Play();
+        if (allParticles.Length == 0 || indexParticlesToPlay >= allParticles.Length)
+        {
+            Debug.LogWarning("ParticlesPlayer attached to " + transform.parent.name + " tried playing nonexistent particles at index " + indexParticlesToPlay + ".");
+            return;
+        }
+
+        allParticles[indexParticlesToPlay].particleSystem.Play();
     }
+
+    public void PlayThenDetachAndDestroy(int indexParticlesToPlay)
+    {
+        if (allParticles.Length == 0 || indexParticlesToPlay >= allParticles.Length)
+        {
+            Debug.LogWarning("ParticlesPlayer attached to " + transform.parent.name + " tried playing nonexistent particles at index " + indexParticlesToPlay + ".");
+            return;
+        }
+
+        transform.parent = null;
+        allParticles[indexParticlesToPlay].particleSystem.Play();
+        StartCoroutine(DestroyWhenFinished(indexParticlesToPlay));
+    }
+    IEnumerator DestroyWhenFinished(int indexParticlesToPlay)
+    {
+        while (allParticles[indexParticlesToPlay].particleSystem.isPlaying)
+            yield return null;
+        Destroy(this.gameObject);
+    }
+
+
+
+    #region Debug
+    [ContextMenu("Test Play First Particles")]
+    void TestPlayFirst() => Play(0);
+    [ContextMenu("Test Play Then Detach and Destroy First Particles")]
+    void TestPlayThenDetachAndDestroyFirst() => PlayThenDetachAndDestroy(0);
+
+    [ContextMenu("Test Play Second Particles")]
+    void TestPlaySecond() => Play(1);
+    [ContextMenu("Test Play Then Detach and Destroy Second Particles")]
+    void TestPlayThenDetachAndDestroySecond() => PlayThenDetachAndDestroy(1);
+
+    [ContextMenu("Test Play Third Particles")]
+    void TestPlayThird() => Play(2);
+    [ContextMenu("Test Play Then Detach and Destroy Third Particles")]
+    void TestPlayThenDetachAndDestroyThird() => PlayThenDetachAndDestroy(2);
+
+    [ContextMenu("Test Play Fourth Particles")]
+    void TestPlayFourth() => Play(3);
+    [ContextMenu("Test Play Then Detach and Destroy Fourth Particles")]
+    void TestPlayThenDetachAndDestroyFourth() => PlayThenDetachAndDestroy(3);
+    #endregion
 }
