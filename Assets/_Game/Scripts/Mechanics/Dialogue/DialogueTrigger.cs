2ae6a7e16e4f30f7dd6a44c978eded9b02db0d7d
JeMarra Rivers <jmonetrivers1@gmail.com>
2021-02-23 15:11:58 -0600

    Dialogue Manager Edits
    created dialogueList Manager

--- Assets/_Game/Scripts/Mechanics/Dialogue/DialogueTrigger.cs
+++ Assets/_Game/Scripts/Mechanics/Dialogue/DialogueTrigger.cs
@@ -4,10 +4,43 @@ using UnityEngine;
 
 public class DialogueTrigger : MonoBehaviour
 {
+    /*
     public Dialogue dialogue;
-
     public void TriggerDialogue()
     {
         FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+    */
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Keypad1))          //random player damage test
+        {
+            TriggerPlayerDamagedDialogue();
+        }
+    }
+
+
+
+    //Static Triggers
+    static public void TriggerPlayerDamagedDialogue()
+    {
+        if (FindObjectOfType<DialogueList>() && FindObjectOfType<DialogueManager>())
+        {
+            DialogueList dialogueListInfo = FindObjectOfType<DialogueList>();
+            if (dialogueListInfo.PlayerDamagedDialogue != null)
+            {
+                int randomDialogue = Random.Range(0, dialogueListInfo.PlayerDamagedDialogue.Length);
+                FindObjectOfType<DialogueManager>().StartDialogue(dialogueListInfo.PlayerDamagedDialogue[randomDialogue]);
+            }
+            else
+            {
+                Debug.Log("DialogueListManager's Player Damage Dialogue List is empty.");
+            }
+
+        }
+        else
+        {
+            Debug.Log("No DialogueListManager or Dialogue Manager in scene.");
+        }
     }
 }
