80a5b26b01ae1f48131b486c205b4de63d3ff71b
Xander <xanderyoussef@gmail.com>
2021-02-28 20:20:38 -0600

    Additional aim fixes for global rotation

---
+++ Assets/_Game/Scripts/Mechanics/Player/Weapons&Aiming/Projectile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+	public float speed = 20f;
+	public int damage = 2;
+	public float lifeTime = 2f;
+	private Rigidbody rb;
+
+	private void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		rb.velocity = transform.forward * speed;
+	}
+
+	private void Awake()
+	{
+		Destroy(gameObject, lifeTime);
+	}
+
+}
--- Assets/_Game/Scripts/Mechanics/Player/Weapons&Aiming/WeaponBase.cs
+++ Assets/_Game/Scripts/Mechanics/Player/Weapons&Aiming/WeaponBase.cs
@@ -6,9 +6,12 @@ public class WeaponBase : MonoBehaviour
 {
 	[Header("Weapon Settings")]
 	public Projectiles projectileType;
+	[SerializeField] GameObject projectile;
+	public Transform[] spawnPoints;
 
 	[Header("Click Fire Settings")]
-	[SerializeField] float clickCooldown = 0.1f;
+	[SerializeField] float clickCooldown = 0.5f;
+	private float cdTime = 0f;
 	[SerializeField] float damage = 3f;
 	[SerializeField] [Range(1,10)] int projectileSpeed = 5;
 	[SerializeField] [Range(0, 180)] float projectileCone = 6f;
@@ -24,19 +27,62 @@ public class WeaponBase : MonoBehaviour
 	[SerializeField] float meterRequired = 50f;
 	[SerializeField] float overloadTime = 2.5f;
 	float chargeMeter = 0f;
-	
-
 
 	void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+	void Update()
+	{
+
+		if (Input.GetButtonDown("Primary Fire"))
+		{
+			switch (projectileType)
+			{
+				case Projectiles.bullet:
+					FireBullet(projectile);
+					break;
+
+				case Projectiles.energy:
+					FireEnergy(projectile);
+					break;
+
+				case Projectiles.laser:
+					FireLaser(projectile);
+					break;
+			}
+		}
+
+	}
+
+
+	void FireBullet(GameObject bullet)
+	{
+		foreach (Transform point in spawnPoints)
+		{
+			Instantiate(bullet, point.position, point.rotation);
+		}
+	}
+
+	// TODO: Add hold click to charge for energy wave
+	void FireEnergy(GameObject wave)
+	{
+		//foreach (Transform point in spawnPoints)
+		//{
+		//	Instantiate(wave, point.position, point.rotation);
+		//}
+	}
+
+	// TODO: Add raycasts for laser firing
+	void FireLaser(GameObject laser)
+	{
+		//foreach (Transform point in spawnPoints)
+		//{
+		//	Instantiate(laser, point.position, point.rotation);
+		//}
+	}
 
 	public enum Projectiles
 	{
