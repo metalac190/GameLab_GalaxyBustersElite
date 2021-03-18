cc6caa777841ce7397b3b07e0af74559911b4497
JeMarra Rivers <jmonetrivers1@gmail.com>
2021-03-18 16:27:11 -0500

    UI and Dialogue Revamp
    -First Draft Dialogue and Static Triggers implemented (DialogueTriggerScript)
    -Main Menu UI Updated
    -Win Screen Updated
    -Lost Screen Updated
    -UI image added and relevant folders created

--- Assets/_Game/Scripts/Mechanics/Dialogue/DialogueManager.cs
+++ Assets/_Game/Scripts/Mechanics/Dialogue/DialogueManager.cs
@@ -8,13 +8,15 @@ public class DialogueManager : MonoBehaviour
 {
 
     private Queue<string> sentences;
-    private Queue<string> dialogueQueue;
+    private Queue<Dialogue> dialogueQueue;
     public GameObject DialoguePopUp;
     public TextMeshProUGUI nameText;
     public TextMeshProUGUI dialogueText;
     public Image NPCImage;
     public float typingDelay = .05f;
+    public float speakerTransionDelay = .5f;
     public float conversationEndDelay = 1.5f;
+    private bool next = false;
 
     void Start()
     {
@@ -31,15 +33,48 @@ public class DialogueManager : MonoBehaviour
         }
         DisplayNexySentence();
     }
+    public void startDialogueArrayFunction(Dialogue[] dialogue)
+    {
+        //StartCoroutine(StartDialogueArray(dialogue));
+        StartCoroutine(StartDialogueStepping(dialogue));
+    }
+    public IEnumerator StartDialogueStepping(Dialogue[] dialogue)
+    {
+        foreach (Dialogue aDialogue in dialogue)
+        {
+            yield return StartCoroutine(StartDialogueCoroutine(aDialogue));
+            while(next == false)
+            {
+                yield return new WaitForSeconds(.2f);
+            }
+            next = false;
+            yield return new WaitForSeconds(speakerTransionDelay);
+        }
+        yield return null;
+    }
+    public IEnumerator StartDialogueCoroutine(Dialogue dialogue)
+    {
+        DialoguePopUp.SetActive(true);
+        nameText.text = dialogue.npcName;
+        sentences.Clear();
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNexySentence();
+        yield return null;
+    }
     public void DisplayNexySentence()
     {
         if (sentences.Count == 0)
         {
             EndDialogue();
+            next = true;
             return;
         }
         string nextSentence = sentences.Dequeue();
-        StopAllCoroutines();
+        //StopAllCoroutines();
+        StopCoroutine("TypeSentence");
         StartCoroutine(TypeSentence(nextSentence));
 
     }
