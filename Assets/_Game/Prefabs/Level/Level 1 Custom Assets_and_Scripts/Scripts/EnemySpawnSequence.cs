a0860ef23a5a6e0d0858f995555f643ffad6dfdd
Benny <liangchengheng1@gmail.com>
2021-03-19 03:29:34 -0500

    Added EnemyDespawn and Spawn Sequence for level designers
    Use these to have a delay spawn of enemies

---
+++ Assets/_Game/Prefabs/Level/Level 1 Custom Assets_and_Scripts/Scripts/EnemySpawnSequence.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnSequence : MonoBehaviour
+{
+    public GameObject[] spawnSections;
+    public bool isImmediate;
+    public float wait_between_time;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //validate, other is Player, probably a better method somewhere
+        if (other.CompareTag("Player"))
+            StartCoroutine(SpawnSequence(isImmediate, wait_between_time));
+
+    }
+
+    IEnumerator SpawnSequence(bool immediate, float time)
+    {
+        for(int i = 0; i < spawnSections.Length; i++)
+        {
+            if(isImmediate == false)
+            {
+                spawnSections[i].SetActive(true);
+                yield return new WaitForSeconds(time);
+            }
+            else
+            {
+                spawnSections[i].SetActive(true);
+            }
+
+
+        }
+
+
+
+
+    }
+
+}
