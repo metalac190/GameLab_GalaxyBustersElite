02821ac26f9108dbf45c3a7b85808e9aa64be27b
Sammy Mahmoudi <bagelmaster5001@gmail.com>
2021-04-06 12:30:18 -0500

    Started implementing close call screen shake

--- Assets/_Game/Scripts/Mechanics/Feedback/Camera/CameraShaker.cs
+++ Assets/_Game/Scripts/Mechanics/Feedback/Camera/CameraShaker.cs
@@ -4,7 +4,7 @@ public class CameraShaker : MonoBehaviour
 {
     public static CameraShaker instance;
 
-    float shakeAmt;
+    public float curShakeAmt;
     const float SHAKE_ENDING_SMOOTHNESS = 20;
     const float NOT_SHAKING_THRESHOLD = 0.01f;
 
@@ -18,8 +18,8 @@ public class CameraShaker : MonoBehaviour
     {
         if (GameManager.gm.Paused) return;
 
-        if (shakeAmt > 0)
-            transform.localPosition = Vector2.up * Random.Range(-shakeAmt, shakeAmt) + Vector2.right * Random.Range(-shakeAmt, shakeAmt);
+        if (curShakeAmt > 0)
+            transform.localPosition = Vector2.up * Random.Range(-curShakeAmt, curShakeAmt) + Vector2.right * Random.Range(-curShakeAmt, curShakeAmt);
         else
             transform.localPosition = Vector2.zero;
     }
@@ -27,13 +27,13 @@ public class CameraShaker : MonoBehaviour
     private void FixedUpdate() => DecrementShakeIfStoppedShaking();
     private void DecrementShakeIfStoppedShaking()
     {
-        if (shakeAmt > NOT_SHAKING_THRESHOLD)
+        if (curShakeAmt > NOT_SHAKING_THRESHOLD)
         {
-            shakeAmt = Mathf.Lerp(shakeAmt, 0, 1 / SHAKE_ENDING_SMOOTHNESS);
-            if (shakeAmt <= NOT_SHAKING_THRESHOLD)
-                shakeAmt = 0;
+            curShakeAmt = Mathf.Lerp(curShakeAmt, 0, 1 / SHAKE_ENDING_SMOOTHNESS);
+            if (curShakeAmt <= NOT_SHAKING_THRESHOLD)
+                curShakeAmt = 0;
         }
     }
 
-    public void Shake(float setShakeAmt) { shakeAmt = setShakeAmt; }
+    public void Shake(float setShakeAmt) { curShakeAmt = setShakeAmt; }
 }
