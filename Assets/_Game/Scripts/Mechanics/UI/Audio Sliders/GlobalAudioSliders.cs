68794950be697f014cd9e750b1bcf7f4ddbb9dc8
Sammy Mahmoudi <bagelmaster5001@gmail.com>
2021-04-26 13:54:10 -0500

    Audio sliders framework (not functional yet)

--- Assets/_Game/Scripts/Mechanics/UI/Audio Sliders/GlobalAudioSliders.cs
+++ Assets/_Game/Scripts/Mechanics/UI/Audio Sliders/GlobalAudioSliders.cs
@@ -1,10 +1,6 @@
 ﻿public static class GlobalAudioSliders
 {
-    public static float masterVolume = 1;
-    public static float musicVolume = 1;
-    public static float soundVolume = 1;
-
-    // Set to true anytime any volume value is changed.
-    // This tells the Sound and Music Players to refresh the volumes of their AudioSources.
-    public static bool anySliderValueChanged = false;
+    public static float masterVolume = 1f;
+    public static float musicVolume = 1f;
+    public static float soundVolume = 1f;
 }
