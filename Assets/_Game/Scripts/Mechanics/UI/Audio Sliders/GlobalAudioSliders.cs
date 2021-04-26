555e79aeaf513ac816f4f8e5cef39e2f1afb66c5
Sammy Mahmoudi <bagelmaster5001@gmail.com>
2021-04-26 12:45:17 -0500

    Created public static script to store audio slider volumes

---
+++ Assets/_Game/Scripts/Mechanics/UI/Audio Sliders/GlobalAudioSliders.cs
@@ -0,0 +1,10 @@
+﻿public static class GlobalAudioSliders
+{
+    public static float masterVolume = 1;
+    public static float musicVolume = 1;
+    public static float soundVolume = 1;
+
+    // Set to true anytime any volume value is changed.
+    // This tells the Sound and Music Players to refresh the volumes of their AudioSources.
+    public static bool anySliderValueChanged = false;
+}
